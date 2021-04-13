5c9f2c139cc66c01b1a96e81fd2a0d9302bf881a
ElenaZhadobina <82103700+ElenaZhadobina@users.noreply.github.com>
2021-04-13 22:33:40 +0500

    Добавлены параметры отвечающие за размеры объектов (size)

--- ZhadobinaElena.cpp
+++ ZhadobinaElena.cpp
@@ -1,13 +1,13 @@
 #include "TXLib.h"
 
 void DrawBackground();
-void DrawSun   (int x, int y);
-void DrawHouse (int x, int y);
-void DrawFence (int x, int y);
-void DrawWood  (int x, int y);
-void DrawCat   (int x, int y);
-void DrawDog   (int x, int y);
-void DrawMouse (int x, int y);
+void DrawFence (int x, int y, double sizeX, double sizeY, COLORREF zaborColor);
+void DrawSun   (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF centreColor, COLORREF luchColor);
+void DrawHouse (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF domColor, COLORREF roofColor, COLORREF windowColor);
+void DrawWood  (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF stvolColor, COLORREF listColor);
+void DrawCat   (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF mordochkaColor);
+void DrawDog   (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF elementyColor);
+void DrawMouse (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF eyasColor);
 
 int main()
     {
@@ -16,26 +16,30 @@ int main()
 
     DrawBackground();
 
-    DrawSun (100, 100);
-    DrawSun (800, 100);
+    DrawSun (100, 100, 1,   1,   1,   TX_YELLOW,   TX_RED);
+    DrawSun (300, 100, 0.7, 0.7, 0.7, TX_RED,      TX_YELLOW);
+    DrawSun (450, 100, 0.5, 0.5, 0.5, TX_LIGHTRED, TX_PINK);
 
-    DrawHouse (650, 340);
+    DrawHouse (650, 340, 1,   1,   1,   TX_ORANGE, TX_DARKGRAY, TX_LIGHTBLUE);
+    DrawHouse (850, 620, 0.8, 0.8, 0.8, TX_CYAN,   TX_ORANGE,   TX_LIGHTBLUE);
 
-    DrawFence (480, 450);
-    DrawFence (400, 450);
-    DrawFence (320, 450);
+    DrawFence (475, 450, 1.1, 1.1, TX_BROWN);
+    DrawFence (385, 450, 1,   1,   TX_RED);
+    DrawFence (305, 450, 0.7, 0.7, TX_YELLOW);
 
-    DrawWood (900, 200);
-    DrawWood (200, 200);
+    DrawWood (900, 120, 1,   1,   1,   TX_BROWN, TX_LIGHTGREEN);
+    DrawWood (150, 220, 0.7, 0.7, 0.7, TX_RED,   TX_YELLOW);
 
-    DrawCat (350, 740);
-    DrawCat (100, 740);
+    DrawCat (100, 740, 0.9, 0.9, 0.9, TX_GRAY,      TX_BLACK);
+    DrawCat (250, 740, 0.5, 0.5, 0.5, TX_LIGHTGRAY, TX_BLACK);
+    DrawCat (350, 740, 0.4, 0.4, 0.4, TX_YELLOW,    TX_BLACK);
 
-    DrawDog (800, 650);
+    DrawDog (650, 500, 0.8, 0.8, 0.8, TX_BROWN,  TX_BLACK);
+    DrawDog (700, 700, 0.6, 0.6, 0.6, TX_ORANGE, TX_BLACK);
 
-    DrawMouse (140, 400);
-    DrawMouse (900, 600);
-    DrawMouse (900, 500);
+    DrawMouse (140, 450, 0.5, 0.5, 0.5, TX_YELLOW, TX_BLACK);
+    DrawMouse (200, 450, 0.6, 0.6, 0.6, TX_RED,    TX_BLACK);
+    DrawMouse (80,  430, 0.7, 0.7, 0.7, TX_ORANGE, TX_BLACK);
     }
 
 void DrawBackground()
@@ -47,202 +51,204 @@ void DrawBackground()
     txRectangle (0, 400, 1000, 800);
     }
 
-void DrawSun (int x, int y)
+void DrawSun (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF centreColor, COLORREF luchColor)
     {
-    txSetFillColor (TX_YELLOW);
-    txCircle (x,    y,    50);
-    txCircle (x-70, y,    10);
-    txCircle (x+70, y,    10);
-    txCircle (x,    y+70, 10);
-    txCircle (x,    y-70, 10);
-    txCircle (x-55, y+50, 10);
-    txCircle (x-55, y-55, 10);
-    txCircle (x+50, y-55, 10);
-    txCircle (x+50, y+50, 10);
+    txSetFillColor (centreColor);
+    txCircle (x, y, 50*sizeR);
+
+    txSetFillColor (luchColor);
+    txCircle (x - 70*sizeX, y,            10*sizeR);
+    txCircle (x + 70*sizeX, y,            10*sizeR);
+    txCircle (x,            y + 70*sizeY, 10*sizeR);
+    txCircle (x,            y - 70*sizeY, 10*sizeR);
+    txCircle (x - 55*sizeX, y + 50*sizeY, 10*sizeR);
+    txCircle (x - 55*sizeX, y - 55*sizeY, 10*sizeR);
+    txCircle (x + 50*sizeX, y - 55*sizeY, 10*sizeR);
+    txCircle (x + 50*sizeX, y + 50*sizeY, 10*sizeR);
     }
 
-void DrawHouse (int x, int y)
+void DrawHouse (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF domColor, COLORREF roofColor, COLORREF windowColor)
     {
     txSetColor     (TX_BLACK, 3);
-    txSetFillColor (TX_ORANGE);
-    txRectangle (x-150, y+110, x+150, y-90);
+    txSetFillColor (domColor);
+    txRectangle (x - 150*sizeX, y + 110*sizeY, x + 150*sizeX, y - 90*sizeY);
 
-    txSetFillColor (TX_LIGHTBLUE);
+    txSetFillColor (windowColor);
+    txRectangle (x + 20*sizeX, y - 40*sizeY, x + 100*sizeX, y + 40*sizeY);
 
-    txRectangle (x+20, y-40, x+100, y+40);
+    txLine (x + 60*sizeX,  y,             x + 100*sizeX, y);
+    txLine (x + 60*sizeX,  y - 40*sizeY,  x + 60*sizeX,  y + 40*sizeY);
+    txLine (x - 160*sizeX, y - 90*sizeY,  x + 160*sizeX, y - 90*sizeY);
+    txLine (x + 160*sizeX, y - 90*sizeY,  x,             y - 240*sizeY);
+    txLine (x,             y - 240*sizeY, x - 160*sizeX, y - 90*sizeY);
 
-    txLine (x+60,  y,     x+100, y);
-    txLine (x+60,  y-40,  x+60,  y+40);
-    txLine (x-160, y-90,  x+160, y-90);
-    txLine (x+160, y-90,  x,     y-240);
-    txLine (x,     y-240, x-160, y-90);
+    txSetFillColor (roofColor);
+    txFloodFill (x, y - 170*sizeY);
 
-    txSetFillColor (TX_DARKGRAY);
-    txFloodFill (x, y-170);
-    txRectangle (x-110, y+110, x-30, y-10);
+    txRectangle (x - 110*sizeX, y + 110*sizeY, x - 30*sizeX, y - 10*sizeY);
 
     txSetFillColor (TX_BLACK);
-    txCircle (x-50, y+60, 5);
+    txCircle (x - 50*sizeX, y + 60*sizeY, 5*sizeR);
     }
 
-void DrawFence (int x, int y)
+void DrawFence (int x, int y, double sizeX, double sizeY, COLORREF zaborColor)
     {
     txSetColor     (TX_BLACK, 3);
-    txSetFillColor (TX_BROWN);
+    txSetFillColor (zaborColor);
 
-    txRectangle (x+20, y, x-20, y-120);
-    txRectangle (x-20, y, x-60, y-120);
+    txRectangle (x + 20*sizeX, y, x - 20*sizeX, y - 120*sizeY);
+    txRectangle (x - 20*sizeX, y, x - 60*sizeX, y - 120*sizeY);
 
-    txLine (x+20, y-120, x,    y-150);
-    txLine (x,    y-150, x-20, y-120);
-    txLine (x-20, y-120, x-40, y-150);
-    txLine (x-40, y-150, x-60, y-120);
+    txLine (x + 20*sizeX, y - 120*sizeY, x,            y - 150*sizeY);
+    txLine (x,            y - 150*sizeY, x - 20*sizeX, y - 120*sizeY);
+    txLine (x - 20*sizeX, y - 120*sizeY, x - 40*sizeX, y - 150*sizeY);
+    txLine (x - 40*sizeX, y - 150*sizeY, x - 60*sizeX, y - 120*sizeY);
 
-    txFloodFill (x-40, y-135);
-    txFloodFill (x,    y-135);
+    txFloodFill (x - 40*sizeX, y - 135*sizeY);
+    txFloodFill (x,            y - 135*sizeY);
     }
 
-void DrawWood (int x, int y)
+void DrawWood (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF stvolColor, COLORREF listColor)
     {
     txSetColor     (TX_BLACK, 3);
-    txSetFillColor (TX_BROWN);
-    txRectangle (x, y, x+40, y+300);
+    txSetFillColor (stvolColor);
+    txRectangle (x, y, x + 40*sizeX, y + 300*sizeY);
 
     txSetColor     (TX_GREEN, 3);
-    txSetFillColor (TX_LIGHTGREEN);
-    txCircle (x+60, y,     30);
-    txCircle (x-30, y+100, 30);
-    txCircle (x+20, y+100, 50);
-    txCircle (x-30, y+50,  50);
-    txCircle (x+50, y+50,  50);
-    txCircle (x+20, y,     50);
-    txCircle (x+70, y+100, 30);
-    txCircle (x-30, y,     30);
+    txSetFillColor (listColor);
+    txCircle (x + 60*sizeX, y,             30*sizeR);
+    txCircle (x - 30*sizeX, y + 100*sizeY, 30*sizeR);
+    txCircle (x + 20*sizeX, y + 100*sizeY, 50*sizeR);
+    txCircle (x - 30*sizeX, y + 50*sizeY,  50*sizeR);
+    txCircle (x + 50*sizeX, y + 50*sizeY,  50*sizeR);
+    txCircle (x + 20*sizeX, y,             50*sizeR);
+    txCircle (x + 70*sizeX, y + 100*sizeY, 30*sizeR);
+    txCircle (x - 30*sizeX, y,             30*sizeR);
     }
 
-void DrawCat (int x, int y)
+void DrawCat (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF mordochkaColor)
     {
     txSetColor     (TX_BLACK, 3);
-    txSetFillColor (TX_GRAY);
+    txSetFillColor (teloColor);
 
-    txLine (x-50,  y+10,  x+100, y+10);
-    txLine (x+100, y+10,  x+25,  y-190);
-    txLine (x+25,  y-190, x-50,  y+10);
+    txLine (x - 50*sizeX,  y + 10*sizeY,  x + 100*sizeX, y + 10*sizeY);
+    txLine (x + 100*sizeX, y + 10*sizeY,  x + 25*sizeX,  y - 190*sizeY);
+    txLine (x + 25*sizeX,  y - 190*sizeY, x - 50*sizeX,  y + 10*sizeY);
 
-    txCircle (x+25, y-160, 50);
-    txCircle (x+25, y-160, 50);
+    txCircle (x + 25*sizeX, y - 160*sizeY, 50*sizeR);
+    txCircle (x + 25*sizeX, y - 160*sizeY, 50*sizeR);
 
-    txLine (x-20, y-180, x-20, y-240);
-    txLine (x-20, y-240, x+10, y-210);
-    txLine (x+70, y-180, x+70, y-240);
-    txLine (x+70, y-240, x+40, y-210);
+    txLine (x - 20*sizeX, y - 180*sizeY, x - 20*sizeX, y - 240*sizeY);
+    txLine (x - 20*sizeX, y - 240*sizeY, x + 10*sizeX, y - 210*sizeY);
+    txLine (x + 70*sizeX, y - 180*sizeY, x + 70*sizeX, y - 240*sizeY);
+    txLine (x + 70*sizeX, y - 240*sizeY, x + 40*sizeX, y - 210*sizeY);
 
-    txFloodFill (x-10, y-220);
-    txFloodFill (x+10, y-40);
-    txFloodFill (x+60, y-200);
+    txFloodFill (x - 10*sizeX, y - 220*sizeY);
+    txFloodFill (x + 10*sizeX, y - 40*sizeY);
+    txFloodFill (x + 60*sizeX, y - 200*sizeY);
 
-    txSetFillColor (TX_BLACK);
+    txSetFillColor (mordochkaColor);
 
-    txCircle (x,    y-170, 5);
-    txCircle (x+40, y-170, 5);
-    txCircle (x+20, y-150, 5);
+    txCircle (x,            y - 170*sizeY, 5*sizeR);
+    txCircle (x + 40*sizeX, y - 170*sizeY, 5*sizeR);
+    txCircle (x + 20*sizeX, y - 150*sizeY, 5*sizeR);
 
-    txLine (x+5,  y-150, x-40, y-150);
-    txLine (x+5,  y-150, x-40, y-160);
-    txLine (x+5,  y-150, x-40, y-140);
-    txLine (x+35, y-150, x+80, y-150);
-    txLine (x+35, y-150, x+80, y-140);
-    txLine (x+35, y-150, x+80, y-160);
+    txLine (x + 5*sizeX,  y - 150*sizeY, x - 40*sizeX, y - 150*sizeY);
+    txLine (x + 5*sizeX,  y - 150*sizeY, x - 40*sizeX, y - 160*sizeY);
+    txLine (x + 5*sizeX,  y - 150*sizeY, x - 40*sizeX, y - 140*sizeY);
+    txLine (x + 35*sizeX, y - 150*sizeY, x + 80*sizeX, y - 150*sizeY);
+    txLine (x + 35*sizeX, y - 150*sizeY, x + 80*sizeX, y - 140*sizeY);
+    txLine (x + 35*sizeX, y - 150*sizeY, x + 80*sizeX, y - 160*sizeY);
 
     txSetFillColor (TX_PINK);
-    txEllipse (x+30, y-130, x+10, y-120);
+    txEllipse (x + 30*sizeX, y - 130*sizeY, x + 10*sizeX, y - 120*sizeY);
 
-    txSetFillColor (TX_GRAY);
+    txSetFillColor (teloColor);
 
-    txEllipse (x-70,  y+10, x-10, y-60);
-    txEllipse (x+120, y+10, x+60, y-60);
+    txEllipse (x - 70*sizeX,  y + 10*sizeY, x - 10*sizeX, y - 60*sizeY);
+    txEllipse (x + 120*sizeX, y + 10*sizeY, x + 60*sizeX, y - 60*sizeY);
 
-    txRectangle (x+3,  y-60, x+20, y);
-    txRectangle (x+33, y,    x+50, y-60);
+    txRectangle (x + 3*sizeX,  y - 60*sizeY, x + 20*sizeX, y);
+    txRectangle (x + 33*sizeX, y,            x + 50*sizeX, y - 60*sizeY);
 
-    txCircle (x+10, y, 15);
-    txCircle (x+43, y, 15);
+    txCircle (x + 10*sizeX, y, 15*sizeR);
+    txCircle (x + 43*sizeX, y, 15*sizeR);
     }
 
-void DrawDog (int x, int y)
+void DrawDog (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF elementyColor)
     {
     txSetColor     (TX_BLACK, 3);
-    txSetFillColor (TX_BROWN);
+    txSetFillColor (teloColor);
 
-    txRectangle (x-200, y+100, x,     y);
-    txRectangle (x-180, y-100, x-100, y-40);
+    txRectangle (x - 200*sizeX, y + 100*sizeY, x,             y);
+    txRectangle (x - 180*sizeX, y - 100*sizeY, x - 100*sizeX, y - 40*sizeY);
 
-    txLine (x-180, y+120, x-130, y+120);
-    txLine (x-130, y+120, x-155, y+80);
-    txLine (x-155, y+80,  x-180, y+120);
+    txLine (x - 180*sizeX, y + 120*sizeY, x - 130*sizeX, y + 120*sizeY);
+    txLine (x - 130*sizeX, y + 120*sizeY, x - 155*sizeX, y + 80*sizeY);
+    txLine (x - 155*sizeX, y + 80*sizeY,  x - 180*sizeX, y + 120*sizeY);
 
-    txSetFillColor (TX_BLACK);
+    txSetFillColor (elementyColor);
 
-    txLine (x-70,  y+120, x-20,  y+120);
-    txLine (x-20,  y+120, x-45,  y+80);
-    txLine (x-45,  y+80,  x-70,  y+120);
-    txLine (x-200, y,     x-220, y+40);
-    txLine (x-220, y+40,  x-250, y+50);
-    txLine (x-250, y+50,  x-240, y+10);
-    txLine (x-240, y+10,  x-200, y);
-    txLine (x-80,  y-130, x-60,  y-170);
-    txLine (x-60,  y-170, x-40,  y-130);
-    txLine (x-40,  y-130, x-80,  y-130);
+    txLine (x - 70*sizeX,  y + 120*sizeY, x - 20*sizeX,  y + 120*sizeY);
+    txLine (x - 20*sizeX,  y + 120*sizeY, x - 45*sizeX,  y + 80*sizeY);
+    txLine (x - 45*sizeX,  y + 80*sizeY,  x - 70*sizeX,  y + 120*sizeY);
+    txLine (x - 200*sizeX, y,             x - 220*sizeX, y + 40*sizeY);
+    txLine (x - 220*sizeX, y + 40*sizeY,  x - 250*sizeX, y + 50*sizeY);
+    txLine (x - 250*sizeX, y + 50*sizeY,  x - 240*sizeX, y + 10*sizeY);
+    txLine (x - 240*sizeX, y + 10*sizeY,  x - 200*sizeX, y);
+    txLine (x - 80*sizeX,  y - 130*sizeY, x - 60*sizeX,  y - 170*sizeY);
+    txLine (x - 60*sizeX,  y - 170*sizeY, x - 40*sizeX,  y - 130*sizeY);
+    txLine (x - 40*sizeX,  y - 130*sizeY, x - 80*sizeX,  y - 130*sizeY);
 
-    txFloodFill (x-155, y+110);
-    txFloodFill (x-155, y+85);
-    txFloodFill (x-45,  y+110);
-    txFloodFill (x-45,  y+85);
-    txFloodFill (x-235, y+40);
-    txFloodFill (x-60,  y-150);
+    txFloodFill (x - 155*sizeX, y + 110*sizeY);
+    txFloodFill (x - 155*sizeX, y + 85*sizeY);
+    txFloodFill (x - 45*sizeX,  y + 110*sizeY);
+    txFloodFill (x - 45*sizeX,  y + 85*sizeY);
+    txFloodFill (x - 235*sizeX, y + 40*sizeY);
+    txFloodFill (x - 60*sizeX,  y - 150*sizeY);
 
-    txSetFillColor (TX_BROWN);
-    txCircle (x-50, y-70, 70);
+    txSetFillColor (teloColor);
+    txCircle (x - 50*sizeX, y - 70*sizeY, 70*sizeR);
 
     txSetFillColor (TX_WHITE);
-    txCircle (x-70, y-95, 20);
+    txCircle (x - 70*sizeX, y - 95*sizeY, 20*sizeR);
 
-    txSetFillColor (TX_BLACK);
+    txSetFillColor (elementyColor);
 
-    txCircle (x-175, y-95, 20);
-    txCircle (x-80,  y-90, 10);
+    txCircle (x - 175*sizeX, y - 95*sizeY, 20*sizeR);
+    txCircle (x - 80*sizeX,  y - 90*sizeY, 10*sizeR);
 
-    txLine (x-35, y-120, x,    y-115);
-    txLine (x,    y-115, x-10, y-155);
-    txLine (x-10, y-155, x-35, y-120);
+    txLine (x - 35*sizeX, y - 120*sizeY, x,            y - 115*sizeY);
+    txLine (x,            y - 115*sizeY, x - 10*sizeX, y - 155*sizeY);
+    txLine (x - 10*sizeX, y - 155*sizeY, x - 35*sizeX, y - 120*sizeY);
 
-    txFloodFill (x-10, y-130);
-    txFloodFill (x-20, y-125);
+    txFloodFill (x - 10*sizeX, y - 130*sizeY);
+    txFloodFill (x - 20*sizeX, y - 125*sizeY);
 
     txSetColor (TX_BLACK, 6);
-    txLine (x-115, y-40, x-180, y-20);
+    txLine (x - 115*sizeX, y - 40*sizeY, x - 180*sizeX, y - 20*sizeY);
     }
 
-void DrawMouse (int x, int y)
+void DrawMouse (int x, int y, double sizeR, double sizeX, double sizeY, COLORREF teloColor, COLORREF eyasColor)
     {
     txSetColor     (TX_BLACK, 2);
-    txSetFillColor (TX_YELLOW);
+    txSetFillColor (teloColor);
 
-    txPie (x-40, y+40, x+40, y+110, 0, 180);
+    txPie (x - 40*sizeX, y + 40*sizeY, x + 40*sizeX, y + 110*sizeY, 0, 180);
 
-    txLine (x+30, y+75,  x+40, y+90);
-    txLine (x+40, y+90,  x+30, y+110);
-    txLine (x+30, y+110, x+50, y+125);
-    txLine (x,    y+30,  x-40, y+55);
-    txLine (x-40, y+55,  x-30, y+5);
-    txLine (x-30, y+5,   x,    y+30);
+    txLine (x + 30*sizeX, y + 75*sizeY,  x + 40*sizeX, y + 90*sizeY);
+    txLine (x + 40*sizeX, y + 90*sizeY,  x + 30*sizeX, y + 110*sizeY);
+    txLine (x + 30*sizeX, y + 110*sizeY, x + 50*sizeX, y + 125*sizeY);
+    txLine (x,            y + 30*sizeY,  x - 40*sizeY, y + 55*sizeY);
+    txLine (x - 40*sizeX, y + 55*sizeY,  x - 30*sizeX, y + 5*sizeY);
+    txLine (x - 30*sizeX, y + 5*sizeY,   x,            y + 30*sizeY);
 
-    txCircle (x-30, y+5,  10);
-    txCircle (x,    y+30, 10);
+    txCircle (x - 30*sizeX, y + 5*sizeY,  10*sizeR);
+    txCircle (x,            y + 30*sizeY, 10*sizeR);
 
-    txFloodFill (x-20, y+40);
+    txFloodFill (x - 20*sizeX, y + 40*sizeY);
 
-    txSetFillColor (TX_BLACK);
-    txCircle (x-25, y+20, 3);
-    txCircle (x-15, y+27, 3);
+    txSetFillColor (eyasColor);
+    txCircle (x - 25*sizeX, y + 20*sizeY, 3*sizeR);
+    txCircle (x - 15*sizeX, y + 27*sizeY, 3*sizeR);
     }
